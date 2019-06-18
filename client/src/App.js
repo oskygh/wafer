14822e498ac530269242466d540afef63fef8c7a
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2019-06-18 14:30:04 +0000

    Reverted to regular bootstrap npm

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Navbar/>
   );
 }
 
