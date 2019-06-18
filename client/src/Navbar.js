71b3a76a6c407b8aee948f446854f397176bb6ac
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2019-06-18 16:00:35 +0000

    Fixed initial Navbar based on Balsamic prototype

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -43,17 +43,12 @@ export default () => (
             </a>
           </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input
-            class="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-            Search
-          </button>
-        </form>
+        <a href="#" className="mr-2">
+            Create Account
+          </a>
+        <a href="#" class="btn btn-outline-success my-2 my-sm-0">
+            Sign In
+          </a>
       </div>
     </nav>
   </div>
