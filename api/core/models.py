b860431abf2bb4875449100af000f163b565d6f5
Iris <osbornem406@gmail.com>
2019-07-02 23:20:05 +0000

    Added Product class and Lender class

--- api/core/models.py
+++ api/core/models.py
@@ -1,18 +1,36 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
-class Retailer (models.Model):
-    #Model
+phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    account_number = models.IntegerField()
-    banker = models.CharField(max_length=100)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+class Lender (models.Model):
+    lender_name = models.CharField(max_length=100)
+    lender_location = models.CharField(max_length=100)
+    lender_phonenumber = models.CharField(validators=[phone_regex], max_length=17)
 
     def __str__(self):
-        return self.name
+        return self.lender_name
 
-   
+class Retailer (models.Model):
+    #Model
+    retailer_name = models.CharField(max_length=100)
+    retailer_location = models.CharField(max_length=100)
+    retailer_account_number = models.IntegerField()
+    retailer_banker = models.ForeignKey(Lender, on_delete=models.CASCADE)
+    retailer_phone_number = models.CharField(validators=[phone_regex], max_length=17)
+
+    def __str__(self):
+        return self.retailer_name
+
+class Product (models.Model):
+    product_name = models.CharField(max_length=100)
+    product_description = models.CharField(max_length=100)
+    product_unitprice = models.DecimalField( max_digits=19, decimal_places=2)
+    product_stock = models.DecimalField( max_digits=19)
+
+    def __str__(self):
+        return self.product_name
+
+
+
 
